8fb9f61fe40d7a42dffbf89479c12700c059c92d
Martin Trobäck <martintk@axis.com>
2021-02-09 16:43:20 +0100

    feat(toasts): export lower level toast components
    To easier create custom toasts we need to export
    the lower level components that are used for the
    predefined toasts.

--- packages/core/src/Toast/index.ts
+++ packages/core/src/Toast/index.ts
@@ -1,3 +1,9 @@
 export * from './toastActions'
 export * from './useToasts'
 export * from './context'
+export {
+  ToastLabel,
+  ToastMessage,
+  ToastIconWrapper,
+  ToastIconType,
+} from './toastCreators'
