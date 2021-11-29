bd914c6b2270fda25f9fb996e2871ccbc0b881e6
Nana Mochihara <nana.mochihara@axis.com>
2021-11-29 12:56:15 +0100

    feat(menu): support submenu functionality

--- packages/core/src/Menu/index.test.tsx
+++ packages/core/src/Menu/index.test.tsx
@@ -40,3 +40,46 @@ describe('Menus', () => {
     expect(tree).toMatchSnapshot()
   })
 })
+
+const ItemsWithSubItems: ReadonlyArray<MenuItemProps> = [
+  {
+    icon: CheckIcon,
+    label: 'Apply policy',
+    submenu: [
+      { icon: CheckIcon, label: 'Sub 1', onClick: clickHandler },
+      { label: 'Sub 2', onClick: clickHandler, disabled: true },
+      { label: 'Sub 3', onClick: clickHandler },
+    ],
+    onClick: clickHandler, //TODO: this should not be needed
+  },
+  { label: 'Enter Credentials', onClick: clickHandler },
+  { label: 'Pause policy', onClick: clickHandler },
+  {
+    label: 'Show device report',
+    onClick: clickHandler,
+    divider: true,
+  },
+  { label: 'Update firmware', onClick: clickHandler, danger: true },
+  { label: 'Restart device', onClick: clickHandler, disabled: true },
+]
+
+describe('Menus with submenu', () => {
+  test('Menu (default)', () => {
+    const tree = TestRender(<Menu items={ItemsWithSubItems} />)
+    expect(tree).toMatchSnapshot()
+  })
+  test('Menu (left)', () => {
+    const tree = TestRender(<Menu items={ItemsWithSubItems} align="left" />)
+    expect(tree).toMatchSnapshot()
+  })
+  test('Menu (right)', () => {
+    const tree = TestRender(<Menu items={ItemsWithSubItems} align="right" />)
+    expect(tree).toMatchSnapshot()
+  })
+  test('Menu (disabled)', () => {
+    const tree = TestRender(
+      <Menu items={ItemsWithSubItems} align="right" disabled={true} />
+    )
+    expect(tree).toMatchSnapshot()
+  })
+})
--- packages/ui-tests/src/coreComponents/Menu.cypress.tsx
+++ packages/ui-tests/src/coreComponents/Menu.cypress.tsx
@@ -19,6 +19,19 @@ export const ITEMS = [
   { label: 'Item 3', disabled: true, danger: true, onClick: NOOP },
 ]
 
+export const ITEMS_WITH_SUBMENU = [
+  {
+    label: 'Enabled item',
+    onClick: NOOP,
+    submenu: [
+      { label: 'Sub 1', onClick: NOOP },
+      { label: 'Sub 2', onClick: NOOP },
+    ],
+  },
+  { label: 'Column options', divider: true, onClick: NOOP },
+  { label: 'Item 3', disabled: true, danger: true, onClick: NOOP },
+]
+
 const Wrapper = styled.div`
   height: 107vh;
   display: flex;
@@ -28,6 +41,7 @@ const Test = () => (
   <Wrapper>
     <Menu items={ITEMS} data-cy="menuTest1" />
     <Menu items={ITEMS} data-cy="menuTest2" />
+    <Menu items={ITEMS_WITH_SUBMENU} data-cy="menuTest_submenu" />
   </Wrapper>
 )
 
