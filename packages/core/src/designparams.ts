fd12bc2bb6aae09842e11d743206de0096962cc0
Martin Trobäck <martin.troback@axis.com>
2022-05-24 08:54:52 +0200

    chore: export designparams as const
    Exports all designparams as const for
    easier hinting of what's included in
    `spacing`, `componentSize` etc.

--- packages/core/src/designparams.ts
+++ packages/core/src/designparams.ts
@@ -6,108 +6,42 @@
  * as components are built to conform to these properties.
  */
 
-interface Spacing {
-  readonly extraSmall: '2px'
-  readonly small: '4px'
-  readonly medium: '8px'
-  readonly large: '16px'
-  readonly extraLarge: '24px'
-  readonly huge: '32px'
-}
-/**
- * Spacing
- * @extraSmall '2px',
- * @small '4px',
- * @medium '8px',
- * @large '16px',
- * @extraLarge '24px',
- * @huge '32px',
- */
-export const spacing: Spacing = {
+export const spacing = {
   extraSmall: '2px',
   small: '4px',
   medium: '8px',
   large: '16px',
   extraLarge: '24px',
   huge: '32px',
-}
+} as const
 
-interface Radius {
-  readonly circle: '50%'
-  readonly small: '2px'
-  readonly medium: '4px'
-  readonly large: '8px'
-}
-interface Shape {
-  readonly radius: Radius
-}
-/**
- * Basic shapes
- */
-export const shape: Shape = {
+export const shape = {
   radius: {
     circle: '50%',
     small: '2px',
     medium: '4px',
     large: '8px',
   },
-}
+} as const
 
-interface ComponentSize {
-  readonly mini: '24px'
-  readonly small: '32px'
-  readonly medium: '40px'
-  readonly large: '48px'
-  readonly extraLarge: '56px'
-}
-/**
- * Component size
- * @mini '24px',
- * @small '32px',
- * @medium '40px',
- * @large '48px',
- * @extraLarge '56px',
- */
-export const componentSize: ComponentSize = {
+export const componentSize = {
   mini: '24px',
   small: '32px',
   medium: '40px',
   large: '48px',
   extraLarge: '56px',
-}
+} as const
 
-interface Iconsize {
-  readonly small: '16px'
-  readonly medium: '24px'
-  readonly large: '40px'
-  readonly extraLarge: '80px'
-}
-/**
- * Icon size
- * @small '16px',
- * @medium '24px',
- * @large '40px',
- * @extraLarge '80px',
- */
-export const iconSize: Iconsize = {
+export const iconSize = {
   small: '16px',
   medium: '24px',
   large: '40px',
   extraLarge: '80px',
-}
+} as const
 
-interface Opacity {
-  readonly 16: 0.16
-  readonly 24: 0.24
-  readonly 40: 0.4
-  readonly 48: 0.48
-}
-/**
- * Opacities
- */
-export const opacity: Opacity = {
+export const opacity = {
   16: 0.16,
   24: 0.24,
   40: 0.4,
   48: 0.48,
-}
+} as const
