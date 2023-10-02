ccee54163e8009880d2f2983cf003479a8be737b
Daniel Karlsson <daniel.karlsson@axis.com>
2023-10-02 09:14:06 +0200

    chore: remove stylelint

--- packages/core/src/utils/twoLinesClamp.ts
+++ packages/core/src/utils/twoLinesClamp.ts
@@ -11,13 +11,9 @@ import { css } from 'styled-components'
  * ```
  */
 export const twoLinesClamp = css`
-  /* stylelint-disable-next-line value-no-vendor-prefix */
   display: -webkit-box;
-  /* stylelint-disable-next-line property-no-vendor-prefix */
   -webkit-line-clamp: 2;
-  /* stylelint-disable-next-line property-no-vendor-prefix */
   -webkit-box-orient: vertical;
-  /* stylelint-disable-next-line property-no-vendor-prefix */
   -moz-box-orient: vertical;
   overflow: hidden;
   white-space: pre-wrap;
